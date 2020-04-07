9ed376f1e9ed45a1c834747564a3ec53b10080aa
radinreth <radin.reth@gmail.com>
2020-04-07 14:40:34 +0700

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ ng build --prod -c ar && \
 ng build --prod -c en && \
 ng build --prod -c ru && \
 ng build --prod -c am && \
-ng build --prod -c qu && \
+ng build --prod -c km_KH && \
+ng build --prod -c es && \
+ng build --prod -c fr && \
 cp CNAME dist/avid-covider/ && \
 git add dist/avid-covider && \
 git commit -m dist && \
